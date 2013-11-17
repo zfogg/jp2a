f70d6ee6bcd8a88aa60009d650d3a73dc694cbf1
Adam Hamot <Rjhoops92@gmail.com>
2013-11-17 10:04:03 -0500

    Added displaying locally

--- src/handle_input.c
+++ src/handle_input.c
@@ -18,11 +18,13 @@ WINDOW * win;
 typedef struct clientframe {
     bool isConnected;
     int new_fd;
+    int disp_fd;
 } Clientframe;
 Clientframe cf;
 
 
 void *startlisten(void*);
+void *getdisplayinfo(void*);
 void reallystartlisten(Clientframe*);
 void sendframe(int, char*, int);
 
@@ -38,6 +40,9 @@ void setupCurses() {
 
     win = newwin(maxY, maxX, 0, 0);
     reallystartlisten(&cf);
+
+    pthread_t tid2;
+    pthread_create(&tid2, NULL, getdisplayinfo, (void *)&cf);
 }
 
 void drawLine(char* s) {
@@ -49,19 +54,25 @@ void drawLine(char* s) {
     }
 }
 
+void *getdisplayinfo(void *arg) {
+  Clientframe *cf = (void*)arg;
+  char* frame = malloc(10000);
+  while (recv(cf->disp_fd, frame, 10000, 0) != -1) {
+    drawLine(frame);
+  }
+}
+
 void killCurses() {
     endwin();
 }
 
 void *startlisten(void *arg) {
     Clientframe *cf = (Clientframe*)arg;
-    struct addrinfo hints;
+    struct addrinfo hints, *res;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC; // use ipv4 or ipv6
     hints.ai_socktype = SOCK_STREAM; // tcp
     hints.ai_flags = AI_PASSIVE; // fill in ip
-
-    struct addrinfo *res;
 
     int status;
     char* port = "9337";
@@ -89,12 +100,20 @@ void *startlisten(void *arg) {
     struct sockaddr_storage their_addr;
     addr_size = sizeof their_addr;
 
-
     int new_fd;
     new_fd = accept(sock, (struct sockaddr *)&their_addr, &addr_size);
+    int disp_fd;
+    disp_fd = accept(sock, (struct sockaddr *)&their_addr, &addr_size);
+
     cf->isConnected = true;
     cf->new_fd      = new_fd;
+    cf->disp_fd     = disp_fd;
 }
+
+// current: client connects to server, server sends frames to client which then sends frames
+// to partner.
+// We receive frames from the partner and send it to the server which displays in terminal.
+// need thread to listen for frames and display them
 
 void reallystartlisten(Clientframe *cf) {
     pthread_t tid;
