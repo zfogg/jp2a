22d06e6c247f8774039e3896d055a3b3abadc97e
Craig Palermo <cpalermo2@gmail.com>
2013-11-20 17:41:03 -0500

    added more comments to client

--- client_server/client.c
+++ client_server/client.c
@@ -15,30 +15,37 @@ int main(int argc, char *argv[])
     char recvBuff[1024];
     struct sockaddr_in serv_addr; 
 
+    // incorrect number of arguments
     if(argc != 2)
     {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
         return 1;
     } 
 
+    // try to open a socket
     memset(recvBuff, '0',sizeof(recvBuff));
+    // error creating socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
         return 1;
     } 
 
+    // reserve memory space to store IP address
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
+    // set type of address to IPV4 and port to 5000
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
 
+    // an error occurred when trying to set server address and port number
     if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
     } 
 
+    // failed when trying to connect to the server 
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Error : Connect Failed \n");
