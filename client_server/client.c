f82c3f7a886edad7e6526a29181060dd92112a6b
Craig Palermo <cpalermo2@gmail.com>
2013-11-20 17:46:44 -0500

    moved some curly braces

--- client_server/client.c
+++ client_server/client.c
@@ -16,8 +16,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr; 
 
     // incorrect number of arguments
-    if(argc != 2)
-    {
+    if(argc != 2) {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
         return 1;
     } 
@@ -25,8 +24,7 @@ int main(int argc, char *argv[])
     // try to open a socket
     memset(recvBuff, '0',sizeof(recvBuff));
     // error creating socket
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Error : Could not create socket \n");
         return 1;
     } 
@@ -39,31 +37,26 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(5000); 
 
     // an error occurred when trying to set server address and port number
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {
         printf("\n inet_pton error occured\n");
         return 1;
     } 
 
     // failed when trying to connect to the server 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
        printf("\n Error : Connect Failed \n");
        return 1;
     } 
 
     /* read from the socket as long as the size of the read is > 0 */
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
         recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
+        if(fputs(recvBuff, stdout) == EOF) {
             printf("\n Error : Fputs error\n");
         }
     } 
 
-    if(n < 0)
-    {
+    if(n < 0) {
         printf("\n Read error \n");
     } 
 
